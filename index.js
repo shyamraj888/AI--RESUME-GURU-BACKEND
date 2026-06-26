9b2668fc5d04e81dceca1c0908338d80d520c1a7
Shyam Rajpurohit <83975056+shyamraj888@users.noreply.github.com>
2026-06-26 13:03:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,14 +19,7 @@ const ai = new GoogleGenAI({
 connectDB();
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    process.env.FRONTEND_URL  // add your Vercel URL here in .env
-  ],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 
